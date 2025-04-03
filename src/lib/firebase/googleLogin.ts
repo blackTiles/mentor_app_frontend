97d8ca8b4b4b509b19fec4d4fd2f6528df0c2f8e
blackTiles <black4tiles@gmail.com>
2025-04-03 11:16:26 +0530

    updated

--- src/lib/firebase/googleLogin.ts
+++ src/lib/firebase/googleLogin.ts
@@ -1,12 +1,26 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { auth } from "@/lib/firebase/config";
+import { API_URL, ENV } from "@/constants/urls";
+import API from "@/lib/axios/instance";
+import { SignOut } from "@/lib/firebase/emailAndPasswordAuth";
 
 const loginWithGoogle = async () => {
+  await SignOut(); // Sign out the user before signing in with Google
   const provider = new GoogleAuthProvider();
   provider.setCustomParameters({ prompt: "select_account" });
   await signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log(result);
+    .then(async () => {   
+      const user = auth.currentUser;
+      const accessToken = await user?.getIdToken().then((accessToken) => {
+        return accessToken;
+      });
+      const response = await API.post(`${API_URL[ENV]}/auth/continue-with-social-account`, {
+        accessToken,
+      });
+      if (response?.data?.success) {
+        console.log(response?.data);
+        return response?.data;
+      }
     })
     .catch((error) => {
       console.log(error);
--- src/lib/firebase/microsoftLogin.ts
+++ src/lib/firebase/microsoftLogin.ts
@@ -2,22 +2,27 @@ import { signInWithPopup, OAuthProvider } from "firebase/auth";
 import { auth } from "@/lib/firebase/config";
 import API from "@/lib/axios/instance";
 import { API_URL, ENV } from "@/constants/urls";
+import { SignOut } from "@/lib/firebase/emailAndPasswordAuth";
 
 const loginWithMicrosoft = async () => {
+  await SignOut(); // Sign out the user before signing in with Microsoft
   const provider = new OAuthProvider("microsoft.com");
   provider.setCustomParameters({
     prompt: "select_account",
   });
   await signInWithPopup(auth, provider)
-    .then((result) => {
-      console.log(result);
-      // const credential = OAuthProvider.credentialFromResult(result);
-      // const accessToken = credential?.accessToken;
-      // const idToken = credential?.idToken;
-      // return {
-      //   accessToken,
-      //   idToken,
-      // };
+    .then(async () => {
+      const user = auth.currentUser;
+      const accessToken = await user?.getIdToken().then((accessToken) => {
+        return accessToken;
+      });
+      const response = await API.post(`${API_URL[ENV]}/auth/continue-with-social-account`, {
+        accessToken,
+      });
+      if (response?.data?.success) {
+        console.log(response?.data);
+        return response?.data;
+      }
     })
     .catch((error) => {
       console.error(error);
