46a028c13abca2ebae35744c8e76d12f9b003896
blackTiles <black4tiles@gmail.com>
2025-04-25 23:35:46 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/layout/Header";
 // import Footer from "./components/layout/Footer";
