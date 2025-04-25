d8e0b498eb773fa83e201cb38dae5c511b7c8296
blackTiles <black4tiles@gmail.com>
2025-04-25 15:41:35 +0530

    updated

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,11 +1,12 @@
-export const ENV = "DEV" 
-// export const ENV = "PROD" 
+// export const ENV = "DEV" 
+export const ENV = "PROD" 
 
 export const API_URL = {
     DEV: "http://localhost:5000/api",
-    PROD: "https://mentor-app-frontend.vercel.app",
+    PROD: "https://mentor-app-backend.vercel.app/api",
 }
 
 export const DOMAIN_URL = {
     DEV: "http://localhost:5173",
+    PROD: "https://mentor-app-frontend.vercel.app",
 }
--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,8 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        // target: "http://localhost:5000",
+        target: "https://mentor-app-backend.vercel.app",
         changeOrigin: true,
       },
     },
